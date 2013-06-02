c35a173cb17218018637c95bb5232353c2b813e3
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-06-02 11:38:15 +0530

    first commit

---
+++ App1.java
@@ -0,0 +1,25 @@
+
+public class App1 {
+	
+	public static void main(String[] args) {
+		
+		int myInt = 7;
+		
+		String text = "HELLO";
+		
+		String blank = " ";
+		
+		String name = "Bob";
+		
+		String greeting = text + blank + name ;
+		
+        System.out.println(greeting);
+        System.out.println("HELLO" + " " + " Bob");
+        System.out.println("My integer is : " + myInt);
+        
+        double myDouble = 7.8d;
+        System.out.println("My number is :" + myDouble + " ");
+		
+	}
+
+}
