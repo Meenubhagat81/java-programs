3ea26895365203936c94443c7f02de51051a95e8
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-06-05 22:37:33 +0530

    add program

---
+++ Pattren.java
@@ -0,0 +1,18 @@
+
+public class Pattren {
+	public static void main(String[] args) {
+		
+		
+		 int k = 2;
+			for (int i = 1; i <= 5; i++) {
+				for (int j = 1; j <= 5; j++) {
+					
+					System.out.print(Math.pow(k, j));
+					
+				}
+				k = k + 1;
+				System.out.print(" \n ");
+			}
+	}
+
+}
