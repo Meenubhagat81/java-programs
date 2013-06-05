9a831913ef706ff267524a22d4cedb5279b3484d
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-06-05 23:20:52 +0530

    add

--- Pattren.java
+++ Pattren.java
@@ -2,14 +2,14 @@
 public class Pattren {
 	public static void main(String[] args) {
 
-		int k = 2;
-		for (int i = 1; i <= 5; i++) {
+		
+		for (int i = 2; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
 
-				System.out.print(Math.pow(k, j));
+				System.out.print(Math.pow(i, j));
 
 			}
-			k = k + 1;
+			
 			System.out.print(" \n ");
 		}
 	}
