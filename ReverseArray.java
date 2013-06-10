3ab678ac75d85d305e1d26ceeb099713f9845e62
Sumit Arora <er.sumitarora@gmail.com>
2013-06-10 12:08:31 -0300

    Updated code with fixes.

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,26 +1,40 @@
+package com.sumitarora;
+
 import java.util.Scanner;
 
-
-public class ReverseArray {
-
+/*
+ * Program to shift array inputs by one place eg.
+ * input array = 1, 2, 3, 4
+ * output array = 2, 3, 4, 1
+ */
+public class RotateArray {
 	
 	public static void main(String[] args) {
-		int SIZE = 3;
-		int[] inputelement = new int[SIZE];
+		
+		int t;
+		int SIZE = 5;
+		int[] inputArray = new int[SIZE];
 
+		//-----------Take user input---------------//
 		Scanner element = new Scanner(System.in);
 		System.out.println("Enter Element : ");
 		for (int i = 0; i < SIZE; i++) {
-			inputelement[i] = element.nextInt();
-			
-		}
-	
-		for (int j = SIZE-1; j >= 0; j--) {
-			System.out.println(inputelement[j]); 
-			
+			inputArray[i] = element.nextInt();			
 		}
 		
-
+		//-----------Shift places---------------//
+		t = inputArray[0];
+		for (int j = 0; j < SIZE; j++) {			
+			if(j == SIZE -1){
+				inputArray[j] = t;				
+			}else{
+				inputArray[j] = inputArray[j+1];
+			}			
+		}
+		
+		//-----------Display outputs---------------//
+		for (int j = 0; j < SIZE; j++) {
+			System.out.println(inputArray[j]);
+		}		
 	}
-
 }
