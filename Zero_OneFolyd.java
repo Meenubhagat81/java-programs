1fe22b0e2e5220b39213e4d620528c70d442fb19
Sumit Arora <er.sumitarora@gmail.com>
2013-06-10 12:15:42 -0300

    Fixed Indentation

--- Zero_OneFolyd.java
+++ Zero_OneFolyd.java
@@ -12,11 +12,11 @@ public class Zero_OneFolyd {
 	
 	public static void main(String[] args) {
 		
-		int i,j,c;		
-		Scanner input = new Scanner(System.in);		
-		System.out.println("Enter the Limit :");		
+		int i,j,c;
+		Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Limit :");
 		int n = input.nextInt();
-		
+	
         c=0;
         for(i=1;i<=n;i++)
         {
