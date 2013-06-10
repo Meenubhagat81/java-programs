6f62287c36cbaa1bea1e682b4d77ec082006247f
Meenu Bhagat <meeeenubhagat87@gmail.com>
2013-06-11 00:35:40 +0530

    StringandSubString

--- StringAddChar.java
+++ StringAddChar.java
@@ -1,27 +1,21 @@
+import java.util.Scanner;
 
 public class StringAddChar {
-	
+
 	public static void main(String[] args) {
-		
-		String text = "Meenu";
-		int len =text.length(); // to find the lenght of string
-		System.out.println("length of string is: "+len);
-		/*
-		char TempCharArray = new char[len];
-		char CharArray = new char[len];
-		
-		for(int i=0; i<len; i++){
-			TempCharArray[i] = text.charAt(i);
-		}
-	
-		for (int j = 0; j < len; j++) {
-            charArray[j] =
-                tempCharArray[len - 1 - j];
-        }
-		String reversePalindrome =
-	            new String(charArray);
-	        System.out.println(reversePalindrome);
-	        */
+            
+		// -----------get string from user----------->>>>>>>
+		Scanner String = new Scanner(System.in);
+		System.out.println("Enter String");
+		String NewString = String.nextLine();
+		System.out.println(NewString);
+		//----------getting length------------------>>>>>>>>
+		int lenght = NewString.length();
+		System.out.println("Lenght of String is :"+ lenght);
+		//---------concatenate---------------------->>>>>>>>
+		String lastChar = NewString.substring(NewString.length()-1);
+		System.out.println(lastChar);
+		System.out.println("Result : "+lastChar+NewString+lastChar);
 	}
 
 }
